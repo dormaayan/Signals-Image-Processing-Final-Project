ff28b315835f8e3ca5dc8bfd3a43664a50b12e6a
dormaayn <dor.d.ma@gmail.com>
2016-10-14 09:54:01 +0300

    Now All Functions Work

--- s4.m
+++ s4.m
@@ -1,5 +1,5 @@
 function [ s ] = s4( A,omega_zero,alpha,phi,t)
 % phi(t) = A * sin(2*pi*t*(omega_zero+alphat^t-1)+phi)
-s =  A * sin(2*pi*t*(omega_zero+alpha.^t-1)+phi);
+s =  A * sin(2*pi*t.*(omega_zero+alpha.^t-1)+phi);
 end
 
