651b78e4638c25af64acba1ab8620eba998139eb
Snorg <shalmon.niv@gmail.com>
2016-12-16 20:56:14 +0200

    Apply fix to checkPhiDependency

--- checkPhiDependency4.m
+++ checkPhiDependency4.m
@@ -6,30 +6,26 @@ for i=1:1:10,
     B = 2048;
     alpha = rand*5 + epsilon;
     omega = rand * 10 + epsilon;
-    floor_omega = floor(omega);
+    floor_omega = ceil(omega);
+    phi = rand*2*pi;
     [b_org,N_org,] = func4Numerically(omega,phi,A,alpha,B);
-    [b_org_floor,N_org_floor,] = func4Numerically(floor_omega,phi,A,floor(alpha),B);
+    [b_org_floor,N_org_floor,] = func4Numerically(floor_omega,phi,A,ceil(alpha),B);
     for j=1:1:10,
         phi = rand*2*pi;
         b = func4Numerically(omega,phi,j,alpha,B);
-        b_floor = func4Numerically(floor(omega),phi,j,floor(alpha),B);
+        b_floor = func4Numerically(ceil(omega),phi,j,ceil(alpha),B);
         if abs(b_floor-b_org_floor)>0.1
             disp('Significant Integer Dependent !!!');
-            break;
         end
         if abs(b_floor-b_org_floor)>epsilon
             disp('Integer Dependent !!!');
-            break;
         end
         if abs(b-b_org)>0.1
             disp('Significant General Dependent !!!');
-            break;
         end
         if abs(b-b_org)>epsilon
             disp('General Dependent !!!');
-            break;
         end
     end
     fprintf('done: %d \n',i);
 end
-
