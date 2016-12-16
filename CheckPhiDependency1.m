f11a0ed8f64d5a0f1b5d88171f9aff751203d389
Snorg <shalmon.niv@gmail.com>
2016-12-16 16:18:42 +0200

    update to scripts, still not working

--- CheckPhiDependency1.m
+++ CheckPhiDependency1.m
@@ -1,27 +1,34 @@
 % Validate whether there is any dependency of the bit allocation problem
 % with the phi in function 1 when omega is integer and when not
 for i=1:1:10,
- A = rand * 15;
- B = 12379;
- omega = rand * 10;
- floor_omega = floor(omega);
- phi = rand*2*pi;
- [b_org,N_org,] = func1Numerically(omega,phi,A,B);
- [b_org_floor,N_org_floor,] = func1Numerically(floor_omega,phi,A,B);
- for j=1:1:10,
-    phi = rand*2*pi;
-    b = func1Numerically( omega,phi,j,B);
-    if abs(b-b_org)>0.001
-        disp('General Dependent !!!');
-    end 
-    if abs(b-b_org)>0.1
-        disp('Significant General Dependent !!!');
-    end 
-    b_floor = func1Numerically(floor(omega),phi,j,B);
-    if abs(b_floor-b_org_floor)>0.001
-        disp('Integer Dependent !!!');
-    end 
- end
- fprintf('done: %d \n',i);
+    epsilon = 0.001;
+    A = rand * 15 + epsilon;
+    B = 2048;
+    omega = rand * 10 + epsilon;
+    floor_omega = floor(omega);
+    [b_org,N_org,] = func1Numerically(omega,phi,A,B);
+    [b_org_floor,N_org_floor,] = func1Numerically(floor_omega,phi,A,B);
+    for j=1:1:10,
+        phi = rand*2*pi;
+        b = func1Numerically( omega,phi,j,B);
+        b_floor = func1Numerically(floor(omega),phi,j,B);
+        if abs(b_floor-b_org_floor)>0.1
+            disp('Significant Integer Dependent !!!');
+            break;
+        end
+        if abs(b_floor-b_org_floor)>epsilon
+            disp('Integer Dependent !!!');
+            break;
+        end
+        if abs(b-b_org)>0.1
+            disp('Significant General Dependent !!!');
+            break;
+        end
+        if abs(b-b_org)>epsilon
+            disp('General Dependent !!!');
+            break;
+        end
+    end
+    fprintf('done: %d \n',i);
 end
 
--- checkPhiDependency5.m
+++ checkPhiDependency5.m
@@ -1,32 +1,36 @@
 % Validate whether there is any dependency of the bit allocation problem
 % with the phi in function 5 when omega is integer and when not
 for i=1:1:10,
- A = rand * 15;
- B = 12379;
- alpha = rand*5;
- beta = rand*3;
- omega = rand * 10;
- floor_omega = floor(omega);
- phi = rand*2*pi;
- [b_org,N_org,] = func5Numerically(omega,phi,A,alpha,beta,B);
- [b_org_floor,N_org_floor,] = func5Numerically(floor_omega,phi,A,floor(alpha),beta,B);
- for j=1:1:10,
-    phi = rand*2*pi;
-    b = func5Numerically(omega,phi,j,alpha,beta,B);
-    if abs(b-b_org)>0.001
-        disp('General Dependent !!!');
-    end 
-    if abs(b-b_org)>0.1
-        disp('Significant General Dependent !!!');
-    end 
-    b_floor = func5Numerically(floor(omega),phi,j,floor(alpha),beta,B);
-    if abs(b_floor-b_org_floor)>0.001
-        disp('Integer Dependent !!!');
-    end 
-    if abs(b_floor-b_org_floor)>0.1
-        disp('Significant Integer Dependent !!!');
-    end 
- end
- fprintf('done: %d \n',i);
+    epsilon = 0.001;
+    A = rand * 15 + epsilon;
+    B = 12379;
+    alpha = rand*5 + epsilon;
+    beta = rand*3 + epsilon;
+    omega = rand * 10 + epsilon;
+    floor_omega = floor(omega);
+    [b_org,N_org,] = func5Numerically(omega,phi,A,alpha,beta,B);
+    [b_org_floor,N_org_floor,] = func5Numerically(floor_omega,phi,A,floor(alpha),beta,B);
+    for j=1:1:10,
+        b_floor = func5Numerically(floor(omega),phi,j,floor(alpha),beta,B);
+        phi = rand*2*pi;
+        b = func5Numerically(omega,phi,j,alpha,beta,B);
+       if abs(b_floor-b_org_floor)>0.1
+            disp('Significant Integer Dependent !!!');
+            break;
+        end
+        if abs(b_floor-b_org_floor)>epsilon
+            disp('Integer Dependent !!!');
+            break;
+        end
+        if abs(b-b_org)>0.1
+            disp('Significant General Dependent !!!');
+            break;
+        end
+        if abs(b-b_org)>epsilon
+            disp('General Dependent !!!');
+            break;
+        end
+    end
+    fprintf('done: %d \n',i);
 end
 
